94e4f98144fef243cbf7f6f42e96d00a479a6711
Ertugrul <iam@ertugrulkat.nl>
2013-05-08 14:47:38 +0200

    right sidebar fixed

---
+++ assets/js/js.js
@@ -0,0 +1,26 @@
+$(function()
+{
+    $("#switch").on("click", function()
+    {
+	   if($("#switch").hasClass("switch"))
+		  {
+			 $(this).removeClass("switch");
+			 $(this).addClass("switch-active");
+		  }
+		  else
+		  {
+			 $(this).addClass("switch");
+			 $(this).removeClass("switch-active");
+			 
+		  }
+    });
+    
+    
+    $("input").on("click",function()
+    {
+	   $("input").parents(".search-bar, .key-field, .user-field").removeClass("focus");
+	   $(this).parents(".search-bar, .key-field, .user-field").addClass("focus");
+
+    });
+    
+});
