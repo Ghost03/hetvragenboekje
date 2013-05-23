dc74e30c156723a9a6a30d6bafc07a3530398c96
Ertugrul <iam@ertugrulkat.nl>
2013-05-23 11:31:57 +0200

    Div login says: shaking my ass

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,57 +1,70 @@
 $(function()
-{
-   $("#inloggen").validate({
-	 errorElement: "div",
-	  errorContainer: $("div.errors"),
-	  errorPlacement: function(error, element) {
-				$(".errors").html("");
-				error.appendTo( element.parent("div").parent("form").parent("div").children(".errors") );
-			},
-	  rules: {
-		 email: {
+    {
+	   $("#inloggen").validate({
+		  errorElement: "div",
+		  errorContainer: $("div.errors"),
+		  errorPlacement: function(error, element) {
+			 $(".errors").html("");
+			 error.appendTo( element.parent("div").parent("form").parent("div").children(".errors") );
+		  },
+		  rules: {
+			 email: {
 				required: true,
 				email: true
 			 },
-		 password: "required"
+			 password: "required"
 			
-	  },
-	  messages: {
-		 email: "Vul een valide e-mailadres in",
-		 password: "Vul een wachtwoord in"
-	  }
-   });
+		  },
+		  messages: {
+			 email: "Vul een valide e-mailadres in",
+			 password: "Vul een wachtwoord in"
+		  }
+	   });
    
-   $("#register").validate({
+	   $("#register").validate({
 	  
-	  errorElement: "div",
-	  errorContainer: $("div.errors"),
-	  errorPlacement: function(error, element) {
-				$(".errors").html("");
-				error.appendTo( element.parent("div").parent("form").parent("div").children(".errors") );
-			},
-	  rules: {
-		 email: {
-			required: true,
-			email: true
-		 },
-		 name: {
-			required: true,
-			minlength: 2
-		 },
-		 rpassword: "required",
-		 rpassword2: {
-			required: true,
-			equalTo: "#rpassword"
-		 }
-	  },
-	  messages: {
-		 name: "Vul een naam in",
-		 email: "Vul een valide e-mailadres in",
-		 rpassword: "Vul een wachtwoord in",
-		 rpassword2: "Wachtwoord komt niet overeen"
-	  }
-   });
-   var emailError = $("#email-error").text();
-  $("#login").children(".errors").append('<div for="email" class="error" style="">'+emailError+'</div>')
+		  errorElement: "div",
+		  errorContainer: $("div.errors"),
+		  errorPlacement: function(error, element) {
+			 $(".errors").html("");
+			 error.appendTo( element.parent("div").parent("form").parent("div").children(".errors") );
+		  },
+		  rules: {
+			 email: {
+				required: true,
+				email: true
+			 },
+			 name: {
+				required: true,
+				minlength: 2
+			 },
+			 rpassword: "required",
+			 rpassword2: {
+				required: true,
+				equalTo: "#rpassword"
+			 }
+		  },
+		  messages: {
+			 name: "Vul een naam in",
+			 email: "Vul een valide e-mailadres in",
+			 rpassword: "Vul een wachtwoord in",
+			 rpassword2: "Wachtwoord komt niet overeen"
+		  }
+	   });
+	   var emailError = $("#email-error").text();
+	   $("#login").children(".errors").append('<div for="email" class="error" style="">'+emailError+'</div>')
+   
+	   var checklogin = $(location).attr('pathname');
+
+	   checklogin.indexOf(2);
+   
+	   checklogin.toLowerCase();
+   
+	   checklogin = checklogin.split("/")[2];
+   
+	   if(checklogin == "checklogin")
+	   {
+		  $("#login").parents(".box").css("height", "280px").effect("shake", 600);
+	   }
    
 });
