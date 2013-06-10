60716c7567c364c9bc38d6dd7700e7e36eb2630a
Ertugrul <iam@ertugrulkat.nl>
2013-06-11 01:45:28 +0200

    info pages

---
+++ private/app/controllers/infoController.php
@@ -0,0 +1,23 @@
+<?php
+
+class InfoController extends AppController {
+
+    public function voorwaardenAction()
+    {   
+
+    }
+     public function overonsAction()
+    {   
+
+    }
+     public function faqAction()
+    {   
+
+    }
+     public function richtlijnenAction()
+    {   
+
+    }
+}
+
+?>
