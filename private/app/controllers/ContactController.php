85126c86931143dbfcd72fa2b6ce95d4718b41a7
Fons Hettema <ffh_93@live.nl>
2013-06-11 06:57:39 +0200

    Contact email

--- private/app/controllers/ContactController.php
+++ private/app/controllers/ContactController.php
@@ -9,32 +9,36 @@ class ContactController extends AppController {
 
     public function contactsendAction()
     {
-    	if(!empty( $_POST['name'] ) && !empty( $_POST['email'] ) && !empty( $_POST['subject'] ) && !empty( $_POST['message'] ) )
-    	{
-    		if(!empty( $_POST['username']) )
-    			$username = trim ( $_POST['username'] );
+        $config = Zend_Registry::get('config');
 
-    		$name = trim( $_POST['name'] );
-    		$email = trim( $_POST['email'] );
-    		$subject = trim( $_POST['subject'] );
-    		$message = trim( $_POST['message'] );
+        if(!empty( $_POST['name'] ) && !empty( $_POST['email'] ) && !empty( $_POST['subject'] ) && !empty( $_POST['message'] ) )
+        {
+            if(!empty( $_POST['username']) )
+                $username = trim ( $_POST['username'] );
 
-			 $auth = array('auth' => 'login',
-				'username' => $config->contactmailer->username,
-				'password' => $config->contactmailer->password);
+            $name = trim( $_POST['name'] );
+            $email = trim( $_POST['email'] );
+            $subject = trim( $_POST['subject'] );
+            $message = trim( $_POST['message'] );
 
-    		$transport = new Zend_Mail_Transport_Smtp($config->contactmailer->smtp, $auth);
-    		$view = Zend_Layout::getMvcInstance()->getView();
-    		$text = $view->partial('mail/contact.phtml', array('name' => $name, 'username' => $username, 'email' => $email, 'subject' => $subject, 'message' => $message));
+             $auth = array('auth' => 'login',
+                'username' => $config->contactmailer->username,
+                'password' => $config->contactmailer->password);
 
-			$mail = new Zend_Mail();
-			$mail->setBodyText($text, 'utf-8');
-			$mail->setBodyHtml($text, 'utf-8');
-			$mail->setFrom($email, $username);
-			$mail->addTo($config->contactmailer->email, 'Het Vragenboekje');
-			$mail->setSubject('Contactformulier - Het Vragenboekje');
-			$mail->send($transport);
-    	}
+            $transport = new Zend_Mail_Transport_Smtp($config->contactmailer->smtp, $auth);
+            $view = Zend_Layout::getMvcInstance()->getView();
+            $text = $view->partial('mail/contact.phtml', array('name' => $name, 'username' => $username, 'email' => $email, 'subject' => $subject, 'message' => $message));
+
+            $mail = new Zend_Mail();
+            $mail->setBodyText($text, 'utf-8');
+            $mail->setBodyHtml($text, 'utf-8');
+            $mail->setFrom($config->contactmailer->email, 'Het Vragenboekje Info');
+            $mail->addTo($config->contactmailer->email, 'Het Vragenboekje Info');
+            $mail->setSubject('Contactformulier - Het Vragenboekje');
+            $mail->send($transport);
+
+            $this->_redirect('contact');
+        }
     }
     
 }
